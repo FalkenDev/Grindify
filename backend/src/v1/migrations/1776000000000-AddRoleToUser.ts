4f470cbcf195cc617c1c7c5a067c0ba4aedbaba6
FalkenDev <falkendev@gmail.com>
2026-05-17 03:01:46 +0200

    feat: add migration to include 'role' column in user table

---
+++ backend/src/v1/migrations/1776000000000-AddRoleToUser.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2026 FalkenDev
+ *
+ * This file is part of Grindify.
+ *
+ * Grindify is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with Grindify. If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class AddRoleToUser1776000000000 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'user',
+      new TableColumn({
+        name: 'role',
+        type: 'varchar',
+        length: '20',
+        default: "'user'",
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('user', 'role');
+  }
+}
